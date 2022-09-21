eb02c81db9f5fc992f2a2abd5a8cf0335b2d3bf4
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-09-20 22:17:53 -0300

    Add files via upload
    matematicas

---
+++ PHP/ej13.php
@@ -0,0 +1,42 @@
+<?php
+    // negación, -
+    $x = 10;
+    echo - $x;
+    echo "<br>";
+
+    // suma, +
+    $x = -10;
+    $y = 8;
+    echo $x + $y;
+    echo "<br>";
+
+    // resta, -
+    $x = 10;
+    $y = 6;
+    echo $x - $y;
+    echo "<br>";
+
+    // multiplicación, *
+    $x = 10;
+    $y = 6;
+    echo $x * $y;
+    echo "<br>";
+
+    // división, /
+    $x = 10;
+    $y = 6;
+    echo $x / $y;
+    echo "<br>";
+
+    // módulo, %
+    $x = 10;
+    $y = 6;
+    echo $x % $y;
+    echo "<br>";
+    
+    // exponente, **
+    $x = 10;
+    $y = 6;
+    echo $x ** $y;
+    echo "<br>";
+  ?>

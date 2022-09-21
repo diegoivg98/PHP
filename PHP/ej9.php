4f688da16eeb1fcff6203162f13bf1ebaf3697dd
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-09-20 22:04:08 -0300

    Add files via upload
    incremento y decremento

---
+++ PHP/ej9.php
@@ -0,0 +1,17 @@
+<?php
+    $x = 10;
+    echo ++$x;
+    echo "<br>";
+
+    $x = 10;
+    echo $x++;
+    echo "<br>";
+
+    $x = 10;
+    echo --$x;
+    echo "<br>";
+    
+    $x = 10;
+    echo $x--;
+    echo "<br>";
+  ?>

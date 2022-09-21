32f41355b3915f398d8ecad1da6bf3fea5daf500
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-09-20 22:02:13 -0300

    Add files via upload
    operadores

---
+++ PHP/ej8.php
@@ -0,0 +1,41 @@
+<?php
+    $x = 100;
+    $y = 50;
+
+    if($x == 100 and $y == 50){
+      echo "Es igual<br>";
+    }else{
+      echo "Es distinto<br>";
+    }
+
+    if($x == 100 && $y == 50){
+      echo "Es igual<br>";
+    }else{
+      echo "Es distinto<br>";
+    }
+
+    if($x == 100 or $y == 50){
+      echo "Es igual<br>";
+    }else{
+      echo "Es distinto<br>";
+    }
+
+    if($x == 100 || $y == 50){
+      echo "Es igual<br>";
+    }else{
+      echo "Es distinto<br>";
+    }
+
+    if($x == 100 xor $y == 50){
+      echo "Es igual<br>";
+    }else{
+      echo "Es distinto<br>";
+    }
+
+    $a = true;
+    if(!$a){
+      echo "Es false<br>";
+    }else{
+      echo "Es true<br>";
+    }
+  ?>

03c8d54bfbd655cefa30277a0088c0e8e3c99cf2
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-09-20 22:13:58 -0300

    Add files via upload
    operadores asignación

---
+++ PHP/ej12.php
@@ -0,0 +1,39 @@
+  <?php
+    $a = 3;
+    $b = $a;
+    echo $a." - ".$b."<br>";
+
+    $b = 6;
+    $b += 3;
+    echo $b."<br>";
+    
+    $b = 6;
+    $b -= 3;
+    echo $b."<br>";
+    
+    $b = 6;
+    $b *= 3;
+    echo $b."<br>";
+    
+    $b = 6;
+    $b /= 3;
+    echo $b."<br>";
+    
+    $b = 6;
+    $b %= 3;
+    echo $b."<br>";
+
+     
+    $a = "Hola";
+    $b = $a;
+    $b .= " Mundo";
+    echo $b."<br>";
+
+    
+    $a = 6;
+    $b = &$a;
+
+    echo "$a + $b + (6+1) = ". ($a+$b+$a+1)."<br>";
+    $c = $a++;
+    echo "$a + $b + $c = ".($a+$b+$c);
+  ?>

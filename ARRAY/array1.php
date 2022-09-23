41b7b7afa1e0a4aa1eda0386c5aaf194d432982f
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-09-23 00:31:45 -0300

    Add files via upload
    ejemplos array

---
+++ ARRAY/array1.php
@@ -0,0 +1,6 @@
+<?php
+/* Creating an array. */
+    $a = array ('a' => 'manzana', 'b' => 'banana', 'c' => array ('x', 'y', 'z'));
+/* Printing the array. */
+    print_r($a);
+  ?>
---
+++ ARRAY/array2.php
@@ -0,0 +1,6 @@
+<?php
+/* Creating an array with three elements. */
+    $a = array (1, 2, ["a", "b", "c"]);
+/* Printing the contents of the array. */
+    var_dump($a);
+  ?>
